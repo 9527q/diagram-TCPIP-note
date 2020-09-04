90913e554a4031e1b78cb3cc1484e78fab07aeae
IkeLiu <liuyu_qi617@163.com>
2020-09-04 16:35:48 +0800

    初始化项目

---
+++ SUMMARY.md
@@ -0,0 +1,1 @@
+* [《图解TCP/IP》第 5 版笔记](README.md)
---
+++ push.sh
@@ -0,0 +1,14 @@
+# 更新文档内容
+gitbook build . docs
+
+# 提交主目录
+git add .
+git commit -m $1
+git push
+
+# 提交文档到 gh-pages 分支
+cd docs
+git init
+git add .
+git commit -m $1
+git push -f git@github.com:9527q/diagram-TCPIP-note.git master:gh-pages
